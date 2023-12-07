bbb432e095e8bebc622590b525dc7f91291c4788
Brent <brent.encarnado98@gmail.com>
2023-12-07 18:10:45 -0500

    added thumbs up guy pic

--- frontend/static/js/Services.js
+++ frontend/static/js/Services.js
@@ -9,9 +9,14 @@ export default class  extends AbstractView {
     async getHtml() {
         return `
             <h1>Services</h1>
+            <div class="rightimage">
+              <img src="../static/images/services.png" height="150" alt="Thumbs up"></a>
+            </div>
             <p>
-             Dedicated space or programs designed to cultivate empathy and understanding among individuals towards the challenges faced by people with disabilities in using technology and interacting with various environments
+             Dedicated space or programs designed to cultivate empathy and understanding among individuals towards the challenges 
+             faced by people with disabilities in using technology and interacting with various environments
             </p>
+            
             <h2>Empathy Lab days and workshops </h2>
             <p>
                 The lab days and workshops typically provide hands-on experiences, simulations, and interactions with assistive technologies, tools, and scenarios that replicate the obstacles individuals with disabilities encounter daily.
@@ -22,7 +27,7 @@ export default class  extends AbstractView {
             </p>
             <h2>Usability testing</h2>
             <p>
-            Go beyond WCAG! Involve individuals with disabilities in usability testing to gather valuable insights for refining product strategy and identifying accessibility concerns at an early stage when solutions are more feasible and cost-effective. You have access to a diverse community of people with disabilities, who use various assistive technologies. With technical expertise ranging from novice to expert, our community can support your product lifecycle from user research, to design, to QA.
+                Go beyond WCAG! Involve individuals with disabilities in usability testing to gather valuable insights for refining product strategy and identifying accessibility concerns at an early stage when solutions are more feasible and cost-effective. You have access to a diverse community of people with disabilities, who use various assistive technologies. With technical expertise ranging from novice to expert, our community can support your product lifecycle from user research, to design, to QA.
             </p>
         `;
     }
