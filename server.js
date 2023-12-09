a2bed83018800737e50cfaa0676837d397ab4a0a
Brent <brent.encarnado98@gmail.com>
2023-12-08 20:35:41 -0500

    trying to publish to github pages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use("/static", express.static(path.resolve(__dirname, "frontend", "static"),
 {extensions:["js"]}));
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
